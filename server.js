25194b467925fc21f4a114ccca3721b8de6828ba
zyreoo <simone.marton89@gmail.com>
2026-01-23 22:05:31 +0200

    change the session to 72 hours

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: { 
-        maxAge: 24 * 60 * 60 * 1000
+        maxAge: 72 * 60 * 60 * 1000 // 72 hours
     }
 }))
 
